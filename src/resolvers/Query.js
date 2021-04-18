c23928d5c7fe2c0789e5f8f901075d15c3cae858
Ibrahim Abdulazeez <waleIbrahim13@gmail.com>
2021-04-18 16:05:26 +0100

    pagination functionality added

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,15 +1,17 @@
-async function feed(parent, arg, context, info) {
-  const where = arg.filter
+async function feed(parent, args, context, info) {
+  const where = args.filter
     ? {
         OR: [
-          { description: { contains: arg.filter } },
-          { url: { contains: arg.filter } },
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
         ],
       }
     : {};
 
   const links = await context.prisma.link.findMany({
     where,
+    skip: args.skip,
+    take: args.take,
   });
   return links;
 }
